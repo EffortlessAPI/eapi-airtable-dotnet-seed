0029c24dde8b5a3bbbd5d336a737ce6021cc4764
Jessica <jessica.lynn.millar@gmail.com>
2020-01-20 15:20:47 -0500

    First pass static GNodeRole for deriving

---
+++ DotNet/GwPlatformLib/Enums/DieselOperatingMode__SimpleDC.cs
@@ -0,0 +1,16 @@
+using System;
+using System.ComponentModel;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using CoreLibrary.Extensions;
+
+namespace GwPlatformDotNet.Lib.Enums
+{                            
+    public enum DieselOperatingMode__SimpleDC
+    {
+        GridConnected,
+        Islanded
+ 
+    }
+}
---
+++ DotNet/GwPlatformLib/Enums/FuzzyLogic.cs
@@ -0,0 +1,17 @@
+using System;
+using System.ComponentModel;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using CoreLibrary.Extensions;
+
+namespace GwPlatformDotNet.Lib.Enums
+{                            
+    public enum FuzzyLogicEnum
+    {
+        TRUE,
+        MAYBE,
+        FALSE
+ 
+    }
+}
